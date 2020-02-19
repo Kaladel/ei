0ba3871d973a408da159ca2258c12afb8627ed28
Floriane Collard <collardfloriane3@gmail.com>
2020-02-19 10:38:15 +0100

    Animation menu, span bleue, loupe du formulaire de recherche

--- scriptkazan.js
+++ scriptkazan.js
@@ -1,33 +1,45 @@
+//sélection du bloc pour l'animation
 let btnBurger = document.querySelector(".animation-div")
+//sélection du menu de navigation
 let sectionBurger = document.querySelector(".menu_nav")
+//sélection des spans pour l'animation de 3 barres en croix
 let span = document.querySelectorAll(".animation-span")
+//sélection du menu général
 let body = document.querySelector(".menu")
 
-/*
-let sectionTopNav = document.querySelector(".section_btnNav_logo")
-let headerdate = document.querySelector(".header_date")
-let figureArticle = document.querySelectorAll(".figure_article")
-*/
 
+//J'ajoute une écoute d'événement sur le bloc qui 
+//s'occupe du menu en mobile
 btnBurger.addEventListener("click", function(){
+    //je lui indique de changer la classe de cache du menu de navigation
     sectionBurger.classList.toggle("hidden")
+    //je lui dit que le menu doit prendre toute la place / ou pas.
     if(body.classList.contains("noscroll")){
         body.classList.remove("noscroll")
     }
     else{
         body.classList.add("noscroll")
     }
+    //j'ajoute la classe crossbar pour effectuer l'animation des span
     span.forEach(element => {
         element.classList.toggle("crossbar")
     });
 
 })    
 
+//On tente de faire pareil qu'au-dessus
+//mais avec le formulaire de recherche
+//On sélectionne la loupe, le label et l'input. 
 let searchBtn = document.querySelector(".fa-search")
 let searchLabel = document.querySelector(".searchlabel")
 let searchInput = document.querySelector(".searchinput")
 
+//pareil qu'en haut, on écoute et on change les classes
+//pour activer ou désactiver la chose
 searchBtn.addEventListener("click", function(){
+    searchBtn.classList.toggle("active")
     searchLabel.classList.toggle("active")
     searchInput.classList.toggle("active")
+    //j'aimerai aussi ajouter un placeholder dans l'input
+    //searchInput.innerHTML = 'Entrez votre recherche';
 }) 
