76844d3fe83ca0299a797a425f32c2a464a270cd
Floriane Collard <collardfloriane3@gmail.com>
2020-02-20 13:25:10 +0100

    tests javascript

--- scriptkazan.js
+++ scriptkazan.js
@@ -44,23 +44,77 @@ searchBtn.addEventListener("click", function(){
     //searchInput.innerHTML = 'Entrez votre recherche';
 }) 
 
+//Reprend tous les li d'activité pour en faire un array
+let screens = document.querySelectorAll(".visit_element");
 
-/* En attendant, je passe sur du hover en css pour 
-voir si tout fonctionne correctement
-//On sélectionne l'encart de la figure, tous les encarts
-let visite = document.querySelectorAll(".visit_overview")
-let description = document.querySelectorAll(".visit_overviw-description")
-
-//On dit qu'on veut que pour chaque élément "visite"
-visite.forEach(elem => {
-    //si l'élément contient "active", on lui retire
-    ev.target.addEventListener("click", function(){
-        if (elem.classList.contains("active")){
-        elem.classList.remove("active");
-        //et on ajoute la classe active à la description
-        description.classList.add("active");
-        }
-        else {elem.classList.add("active")}
-    })
+//écoute le mouvement de la fenêtre
+window.addEventListener("mouseover", function(){
+    //on dit que pour chaque li activité il agit sur l'écran
+    screens.forEach(screen =>{
+        screen.addEventListener("click", function(){
+            let topEl = screen.offsetTop;
+            window.scrollTo({top:topEl})
+        })
+        })
 })
+
+//https://cepegra.yo.fr/kazan_api/
+//cdnjs axios
+//https://cdnjs.cloudflare.com/ajax/libs/axios/0.19.2/axios.min.js
+/*
+const Main = function () {
+      axios.get(url)
+      .then(response => {
+          XXXX = response.data
+      })
+      .then(Affiche)
+      .catch(function() {
+          alert('erreur de connexion')
+      })
+  }
+window.addEventlistener("load", Main)
 */
+
+
+let allRequireds = document.querySelectorAll("[required]")
+let btn = document.querySelector("input[disabled]")
+//console.log(allRequireds)
+let test = true
+
+allRequireds.forEach(required => {
+    required.addEventListener("change", function() {
+        //alert('test')
+        // test des autres champs
+        // impossible de faire un break sur foreach => for
+        for (let i = 0; i < allRequireds.length; i++) {
+            let theField = allRequireds[i] //recupération d'un champ
+            
+            if ( theField.value == '') // si la valeur est vide
+            {
+                test = false
+                break //sortir de la boucle
+            }
+            else if (theField.type == 'email') { //si pas vide mais email
+                //alert('email')
+                let expressionReguliere = /^(([^<>()[]\.,;:s@]+(.[^<>()[]\.,;:s@]+)*)|(.+))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/ //pattern
+                if (!expressionReguliere.test(theField.value)) { //si value différent du pattern
+                    test = false
+                    break //sortir boucle
+                }
+
+            }
+            else { //value ok
+                test = true
+            }
+        }
+
+        if ( test == true) { 
+            btn.removeAttribute('disabled') //active le btn en retirant attriut desabled
+        }
+        else {
+            btn.setAttribute('disabled', true) // désactive le btn en ajoutant l'attribut desabled
+        }
+        //  console.log(test)
+    })
+});
+    
---
+++ testjs.js
@@ -0,0 +1,24 @@
+//https://cepegra.yo.fr/kazan_api/
+
+/*
+innerHtml
+<li>
+  <figure class="visit_overview">
+        <img src="src/images/hermitage-kazan-russia.jpg" alt="Hermitage de Kazan" class="visit_image">
+        <figcaption>
+            <h3 class="visit_title"><strong>Kazan </strong>Hermitage</h3>
+            <p class="visit_location">Center of the city</p>
+        </figcaption>
+    </figure>
+    <div class="visit_overview-description">
+        <h3 class="visit_title"><strong>Kazan </strong>Hermitage</h3>
+        <h4 class="visit_subtitle">Palais des congrès et centre d'expositions</h4>
+        <p class="visit_description"></p>
+        <div>
+            <i class="fa fa-heart"></i>
+            <a href="#" title="En lire plus">Read more</a>
+        </div>
+        <a href="#" title="En savoir plus">...</a>
+    </div>  
+</li>
+*/
